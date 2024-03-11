4cee25a6a76b32c6d3df92abfc4bbb01fd970e74
Umesh Sarma <usarma1200@gmail.com>
2024-03-11 09:43:09 -0500

    Initial Commit

---
+++ API Calling/API_CallingApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_CallingApp.swift
+//  API Calling
+//
+//  Created by Umesh Sarma on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct API_CallingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
